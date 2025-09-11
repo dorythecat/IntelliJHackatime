47e22228b8bad3b6e361897b0a51d54fcd91356c
dorythecat <pemegomachado@gmail.com>
2025-09-11 11:42:33 +0200

    Added basic configure action (not implemented rn)

---
+++ src/main/kotlin/io/github/dorythecat/intellijhackatime/Configure.kt
@@ -0,0 +1,10 @@
+package io.github.dorythecat.intellijhackatime
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class Configure : AnAction() {
+    override fun actionPerformed(p0: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
