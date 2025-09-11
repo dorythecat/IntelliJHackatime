8657961775f176ad96bd928a5062a16d283b6c5d
dorythecat <pemegomachado@gmail.com>
2025-09-11 20:26:24 +0200

    Added info loading to startup

--- src/main/kotlin/io/github/dorythecat/intellijhackatime/IntelliJHackatime.kt
+++ src/main/kotlin/io/github/dorythecat/intellijhackatime/IntelliJHackatime.kt
@@ -1,10 +1,17 @@
 package io.github.dorythecat.intellijhackatime
 
-import com.intellij.openapi.startup.StartupActivity
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
 
 class IntelliJHackatime : StartupActivity {
+    var VERSION: String? = null
+    var IDE_NAME: String? = null
+    var IDE_VERSION: String? = null
+
     override fun runActivity(project: Project) {
-        println("Test")
+        VERSION = this.javaClass.`package`.implementationVersion
+        IDE_NAME = System.getProperty("idea.platform.prefix")
+        IDE_VERSION = System.getProperty("idea.version")
+        println("Hackatime v$VERSION running on $IDE_NAME v$IDE_VERSION")
     }
 }
