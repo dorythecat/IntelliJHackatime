c1efcd3de144e7af0f57595626e92f84d964f10a
dorythecat <pemegomachado@gmail.com>
2025-09-12 11:27:57 +0200

    Fixed a few issues

--- src/main/kotlin/io/github/dorythecat/intellijhackatime/IntelliJHackatime.kt
+++ src/main/kotlin/io/github/dorythecat/intellijhackatime/IntelliJHackatime.kt
@@ -1,11 +1,15 @@
 package io.github.dorythecat.intellijhackatime
 
+import com.intellij.ide.plugins.PluginManager
+import com.intellij.ide.plugins.PluginManagerCore.getPlugin
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import java.io.File
 import java.math.BigDecimal
 import java.math.MathContext
+
 
 var VERSION: String = ""
 var IDE_NAME: String = ""
@@ -23,7 +27,13 @@ class IntelliJHackatime : ProjectActivity {
     private val dependencies = Dependencies()
 
     override suspend fun execute(project: Project) {
-        VERSION = this.javaClass.`package`.implementationVersion
+        try {
+            // support older IDE versions with deprecated PluginManager
+            VERSION = PluginManager.getPlugin(PluginId.getId("io.github.dorythecat.IntelliJHackatime"))!!.version
+        } catch (e: Exception) {
+            // use PluginManagerCore if PluginManager deprecated
+            VERSION = getPlugin(PluginId.getId("io.github.dorythecat.IntelliJHackatime"))!!.version
+        }
         IDE_NAME = System.getProperty("idea.platform.prefix")
         IDE_VERSION = System.getProperty("idea.version")
         log.info("Hackatime v$VERSION running on $IDE_NAME v$IDE_VERSION")
